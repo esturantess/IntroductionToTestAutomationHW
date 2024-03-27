daf46759aa56a4daf125acb17cd4e254ba4e399f
Polina <ms.maylin@inbox.ru>
2024-03-28 02:42:07 +0300

    deleted extra extends in SingleVisitorTest

--- src/test/java/org/max/HW2/SingleVisitorTest.java
+++ src/test/java/org/max/HW2/SingleVisitorTest.java
@@ -5,7 +5,7 @@ import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.ValueSource;
 
-public class SingleVisitorTest extends AbstractTestInitVisitors{
+public class SingleVisitorTest{
 
     @ParameterizedTest
     @ValueSource(doubles = {0.00, 49999.99})
